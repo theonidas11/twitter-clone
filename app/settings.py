efe283852f73eb8bb9f044f2c1d3af730e6e949e
Theo <rockemted@gmail.com>
2022-09-19 23:23:31 -0700

    modified whitenoise

--- app/settings.py
+++ app/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
    # BASE_DIR / 'static'
 #]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = "whitenoise.storge.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 cloudinary.config( 
