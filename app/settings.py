2a536d9ec371080554c92eed050ddda9e8f852fe
Theo <rockemted@gmail.com>
2022-09-21 19:36:06 -0700

    updadted issues

--- app/settings.py
+++ app/settings.py
@@ -118,20 +118,28 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-STATIC_URL = 'static/'
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
-#STATICFILES_DIRS = [
-   # BASE_DIR / 'static'
-#]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 
 cloudinary.config( 
--- posts/views.py
+++ posts/views.py
@@ -25,6 +25,7 @@ def index(request):
    
     # Get all posts, limit =20
     posts = Post.objects.all().order_by('-created_at')[:20]
+    form = PostForm()
 
     # Show
     return render(request, 'post.html',
@@ -38,15 +39,18 @@ def delete(request, post_id):
     return HttpResponseRedirect('/')   
 
 def edit(request, post_id):
-    post = Post.objects.get(id=post_id)
+    if request.method == 'GET':
+        posts = Post.objects.get(id=post_id)
+        return render(request, 'edit.html', {"posts": posts})
     if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES, instance=post)
+        editposts = Post.objects.get(id=post_id)
+        form = PostForm(request.POST, request.FILES, instance=editposts)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/')
 
         else: 
-            return HttpResponseRedirect(form.errors.as_json())
+            return HttpResponseRedirect("not valid")
 
     form = PostForm 
     return render(request, 'edit.html', {'posts':post, 'form':form})  
